31eb70db6f10832381aada43832e0c1e3fb91e8d
Joel Papineau <118376438+j-papineau@users.noreply.github.com>
2024-11-01 12:16:48 -0400

    moving computers

--- ui-team-project/src/app/(main-pages)/ViewJob/page.tsx
+++ ui-team-project/src/app/(main-pages)/ViewJob/page.tsx
@@ -1,14 +1,20 @@
-import ViewJobPage from '@/app/pages/ViewJobPage'
+import ButtonTest from '@/app/components/Header/ButtonTest'
+import Header from '@/app/components/Header/Header'
 import React from 'react'
 
 type Props = {}
 
-const page = (props: Props) => {
+const ViewJob = (props: Props) => {
+
+
+
   return (
-    <div>
-        <ViewJobPage/>
+    <div className='flex flex-col items-center'>
+      <Header titleText='UNFinished Business Job Portal'/>
+      <ButtonTest />
     </div>
   )
 }
 
-export default page
+
+export default ViewJob
